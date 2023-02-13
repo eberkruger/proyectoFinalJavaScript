c309b443aea27c6d4778e98a207864df4da006d1
eberkruger <eberkruger@hotmail.com.ar>
2023-02-12 23:47:05 -0300

    Corrección de estilo

---
+++ js/carrito.js
@@ -0,0 +1,179 @@
+let todosProductosCarrito = localStorage.getItem("productosEnCarrito");
+todosProductosCarrito = JSON.parse(todosProductosCarrito);
+const carritoVacio = document.getElementById("carritoVacio");
+const productosCarrito = document.getElementById("productosCarrito");
+const accionesCarrito = document.getElementById("accionesCarrito");
+const carritoComprado = document.getElementById("carritoComprado");
+let botonesEliminar = document.getElementsByClassName(
+  "carritoProductoEliminar"
+);
+let botonVaciar = document.getElementById("accionesCarritoVaciar");
+const precioCarritoTotal = document.getElementById("total");
+let botonComprar = document.getElementById("accionesCarritoComprar");
+
+function renderizarCarrito() {
+  if (todosProductosCarrito && todosProductosCarrito.length > 0) {
+    carritoVacio.classList.add("disabled");
+    productosCarrito.classList.remove("disabled");
+    accionesCarrito.classList.remove("disabled");
+
+    productosCarrito.innerHTML = "";
+
+    todosProductosCarrito.forEach(({imgUrl, nombre, cantidad, precio, id}) => {
+      let productoAgregadoCarrito = document.createElement("div");
+      productoAgregadoCarrito.classList.add("productoAgregado");
+      productoAgregadoCarrito.innerHTML = `
+                <img class="productoAgragadoImagen" src="${imgUrl}" alt="${nombre}">
+                <div class="carritoProductoNombre">
+                  <small>Nombre</small>
+                  <h4>${nombre}</h4>
+                </div>
+                <div class="botonesCantidades">
+                  <button id=inc${id} onclick=incrementarUnidad(${id})><img src="../assets/imagenesSVG/add-outline.svg"/></button>
+                  <button id=dec${id} onclick=decrementarUnidad(${id})><img src="../assets/imagenesSVG/remove-outline.svg"/></button>
+                </div>
+                <div class="carritoProductoCantidad">
+                  <small>Cantidad</small>
+                  <p>${cantidad}</p>
+                </div>
+                <div class="carritoProductoPrecio">
+                  <small>Precio</small>
+                  <p>${precio}</p>
+                </div>
+                <div class="carritoProductoSubtotal">
+                  <small>Subtotal</small>
+                  <p>${precio * cantidad}</p>
+                </div>
+                <button id="${id}"class="carritoProductoEliminar"><lord-icon
+                src="https://cdn.lordicon.com/jmkrnisz.json"
+                trigger="hover"
+                colors="primary:#e83a30"
+                style="width:32px;height:32px">
+                </lord-icon></button>
+            `;
+
+      productosCarrito.appendChild(productoAgregadoCarrito);
+    });
+
+    ActualizarBotonesEliminar();
+    precioTotal();
+  } else {
+    carritoVacio.classList.remove("disabled");
+    productosCarrito.classList.add("disabled");
+    accionesCarrito.classList.add("disabled");
+  }
+}
+renderizarCarrito();
+
+function decrementarUnidad(id) {
+  let indexProducto = todosProductosCarrito.findIndex(producto => producto.id == id);
+  if (todosProductosCarrito[indexProducto].cantidad > 1) {
+    todosProductosCarrito[indexProducto].cantidad--
+  } else {
+    todosProductosCarrito.splice(indexProducto, 1)
+  };
+  renderizarCarrito(todosProductosCarrito);
+  localStorage.setItem("productosEnCarrito", JSON.stringify(todosProductosCarrito));
+}
+
+function incrementarUnidad(id) {
+  let indexProducto = todosProductosCarrito.findIndex(producto => producto.id == id);
+  todosProductosCarrito[indexProducto].cantidad++;
+
+  renderizarCarrito(todosProductosCarrito);
+  localStorage.setItem("productosEnCarrito", JSON.stringify(todosProductosCarrito));
+}
+
+function ActualizarBotonesEliminar() {
+   let botonesEliminar = document.querySelectorAll(".carritoProductoEliminar");
+
+  botonesEliminar.forEach((boton) => {
+    boton.addEventListener("click", eliminarDelCarrito);
+  });
+}
+
+function eliminarDelCarrito(e) {
+  const id = e.currentTarget.id;
+  const index = todosProductosCarrito.findIndex((producto) => producto.id == id);
+
+  todosProductosCarrito.splice(index, 1);
+  renderizarCarrito();
+  Toastify({
+    text: "Producto eliminado del carrito",
+    className: "toastify",
+    duration: 1500,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #122923, #e73958)",
+    },
+  }).showToast();
+  localStorage.setItem("productosEnCarrito", JSON.stringify(todosProductosCarrito));
+}
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+function vaciarCarrito() {
+  todosProductosCarrito.length = 0;
+  localStorage.setItem("productosEnCarrito", JSON.stringify(todosProductosCarrito));
+  renderizarCarrito();
+  lanzarSweetAlert(
+    "Carrito vacío",
+    "Siga reccoriendo la tienda",
+    "success",
+    2000,
+    false,
+    "#e73958"
+  );
+}
+
+function precioTotal() {
+  const totalCalculadoProductos = todosProductosCarrito.reduce((acc, producto) => acc + producto.precio * producto.cantidad,0);
+  precioCarritoTotal.innerText = `$${totalCalculadoProductos}`;
+}
+
+botonComprar.addEventListener("click", comprarCarrito);
+function comprarCarrito() {
+  todosProductosCarrito.length = 0;
+  localStorage.setItem(
+    "productosEnCarrito",
+    JSON.stringify(todosProductosCarrito)
+  );
+
+  carritoVacio.classList.toggle("disabled");
+  productosCarrito.classList.toggle("disabled");
+  accionesCarrito.classList.toggle("disabled");
+  /* sweet alert */
+  lanzarSweetAlert(
+    "Gracias por su compra",
+    "Vuelva pronto",
+    "success",
+    2000,
+    false,
+    "#e73958"
+  );
+}
+
+function lanzarSweetAlert(
+  title,
+  text,
+  icon,
+  timer,
+  mostrarBoton,
+  colorIcono,
+  imageUrl,
+  imageWidth,
+  imageHeight
+) {
+  Swal.fire({
+    icon,
+    title,
+    text,
+    timer,
+    showConfirmButton: mostrarBoton,
+    iconColor: colorIcono,
+    imageUrl,
+    imageWidth,
+    imageHeight,
+  });
+}
---
+++ js/main.js
@@ -0,0 +1,180 @@
+async function pedirInfo() {
+  const resp = await fetch("../json/productos.json")
+  const productosDB = await resp.json()
+  miPrograma(productosDB)
+}
+pedirInfo()
+
+function miPrograma(productos) {
+
+  let carrito = localStorage.getItem("productosEnCarrito") ? JSON.parse(localStorage.getItem("productosEnCarrito")) : [];
+
+  const contenedorProductos = document.getElementById("contenedorProductos");
+
+  function renderizarProductos(arrayDeProductos) {
+    contenedorProductos.innerHTML = "";
+
+    arrayDeProductos.forEach(({ id, precio, nombre, imgUrl }) => {
+      let tarjetaProducto = document.createElement("div");
+      tarjetaProducto.classList.add("card");
+      tarjetaProducto.innerHTML = `
+          <img src="${imgUrl}" alt="${nombre}">
+          <div class="descripcion">
+            <p class="nombreProducto">${nombre}</p>
+            <p class="precioProducto">$${precio}</p>
+            <button class="agregarCarrito" id="${id}">
+            <img src="../assets/imagenesSVG/bxs-cart.svg" alt="Carrito de compras"/>
+            </button>
+          </div>
+        `;
+
+      contenedorProductos.appendChild(tarjetaProducto);
+    });
+
+    ActualizarBotonesAgregar();
+  }
+  renderizarProductos(productos);
+
+  /* ******** BARRA BUSCADORA ******* */
+  const buscador = document.getElementById("buscador");
+  const buscar = document.getElementById("botonBuscar");
+  const hero = document.getElementById("hero");
+  const menuFiltros = document.getElementById("menuFiltros");
+  buscar.onclick = filtrarNombre;
+
+  function filtrarNombre() {
+    let filtrarNombreProducto = productos.filter(
+      ({ nombre, categoria }) =>
+        nombre.toLowerCase().includes(buscador.value.toLowerCase()) ||
+        categoria.toLowerCase().includes(buscador.value.toLowerCase())
+    );
+
+    apretarEnter();
+    renderizarProductos(filtrarNombreProducto);
+
+    hero.classList.toggle("disabled");
+    menuFiltros.classList.toggle("disabled");
+    tituloPrincipal.classList.toggle("disabled");
+  }
+
+  function apretarEnter() {
+    buscador.addEventListener("keyup", function (e) {
+      if (e.code === "Enter") {
+        buscar.click();
+        hero.classList.toggle("disabled");
+        menuFiltros.classList.toggle("disabled");
+        tituloPrincipal.classList.toggle("disabled");
+      }
+    });
+  }
+
+  /* ******** SECTION CATEGORÍAS ********  */
+  /* TODOS */
+  const tituloPrincipal = document.getElementById("tituloPrincipal");
+  const verTodosProductos = document.getElementById("todos");
+  verTodosProductos.addEventListener("click", mostrarTodosProductos);
+
+  function mostrarTodosProductos(e) {
+    if (e.target.id == "todos") {
+      tituloPrincipal.innerText = "Todos nuestros productos";
+      renderizarProductos(productos);
+    }
+  }
+
+  /* NOTEBOOKS */
+  const botonCategoriaNotebook = document.getElementById("notebook");
+  botonCategoriaNotebook.addEventListener("click", filtrarCategoriaNotebook);
+  
+  function filtrarCategoriaNotebook(e) {
+    if (e.target.id == "notebook") {
+      tituloPrincipal.innerText = "Notebooks";
+      let filtrarNotebook = productos.filter(
+        ({ categoria }) => categoria.toLowerCase() === e.target.id
+      );
+      renderizarProductos(filtrarNotebook);
+    }
+  }
+
+  /* MOUSES */
+  const botonCategoriaMouse = document.getElementById("mouse");
+  botonCategoriaMouse.addEventListener("click", filtrarCategoriaMouse);
+  function filtrarCategoriaMouse(e) {
+    if (e.target.id == "mouse") {
+      tituloPrincipal.innerText = "Mouses";
+      let filtrarMouse = productos.filter(
+        ({ categoria }) => categoria.toLowerCase() === e.target.id
+      );
+      renderizarProductos(filtrarMouse);
+    }
+  }
+
+  /* TECLADOS */
+  const botonCategoriaTeclado = document.getElementById("teclado");
+  botonCategoriaTeclado.addEventListener("click", filtrarCategoriaTeclado);
+
+  function filtrarCategoriaTeclado(e) {
+    if (e.target.id == "teclado") {
+      tituloPrincipal.innerText = "Teclados";
+      let filtrarTeclado = productos.filter(
+        ({ categoria }) => categoria.toLowerCase() === e.target.id
+      );
+      renderizarProductos(filtrarTeclado);
+    }
+  }
+
+  /* AURUCULARES */
+  const botonCategoriaAuricular = document.getElementById("auricular");
+  botonCategoriaAuricular.addEventListener("click", filtrarCategoriaAuricular);
+
+  function filtrarCategoriaAuricular(e) {
+    if (e.target.id == "auricular") {
+      tituloPrincipal.innerText = "Auriculares";
+      let filtrarAuricular = productos.filter(
+        ({ categoria }) => categoria.toLowerCase() === e.target.id
+      );
+      renderizarProductos(filtrarAuricular);
+    }
+  }
+
+  /* ACTUALIZAR BOTONES DE AGREGAR AL CARRITO */
+  function ActualizarBotonesAgregar() {
+    let botonesAgregarCarrito = document.querySelectorAll(".agregarCarrito");
+
+    botonesAgregarCarrito.forEach((boton) => {
+      boton.addEventListener("click", agregarAlCarrito);
+    });
+  }
+
+  /* FUNCION AGREGAR AL CARRITO */
+  function agregarAlCarrito(e) {
+    let id = e.currentTarget.id;
+    const productoAgregado = productos.find((producto) => producto.id == id);
+    
+    lanzarToastify()
+
+    if (carrito.some((producto) => producto.id == id)) {
+      const index = carrito.findIndex((producto) => producto.id == id);
+      carrito[index].cantidad++;
+    } else {
+      productoAgregado.cantidad = 1;
+      carrito.push(productoAgregado);
+    }
+
+    localStorage.setItem("productosEnCarrito", JSON.stringify(carrito));
+  }
+
+  function lanzarToastify() {
+    Toastify({
+      text: "Producto agregado al carrito",
+      className: "toastify",
+      duration: 1500,
+      gravity: "bottom", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #122923, #e73958)",
+      },
+    }).showToast();
+  }
+}
+
